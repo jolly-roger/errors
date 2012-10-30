5885438dfc67219654a96a3f7eb8c454c28359a8
roger <igor.onyshchenko@gmail.com>
2012-10-30 10:50:15 +0200

    i

---
+++ errors.py
@@ -0,0 +1,38 @@
+import cherrypy
+import smtplib
+from email.mime.text import MIMEText
+
+
+def sendmail(status, message, traceback, version, subject, cprequest):
+    sender = 'www@dig-dns.com (www)'
+    recipient = 'roger@dig-dns.com'
+    
+    text = 'Base: ' + cprequest.base + '\n\n' +\
+        'Request line: ' + cprequest.request_line + '\n\n' +\
+        'Status: ' + status + '\n\n' + 'Message: ' + message + '\n\n' +\
+        'Traceback: ' + traceback + '\n\n' + 'Version: ' + version
+    
+    msg = MIMEText(text)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail(sender, recipient, msg.as_string())
+    s.quit()
+
+
+class robots(object):
+    @cherrypy.expose
+    def sendmail(self, status, message, traceback, version, subject, cprequest):
+        return sendmail(status, message, traceback, version, subject, cprequest)
+
+
+def error_page_default(status, message, traceback, version):
+    return sendmail(status, message, traceback, version, 'Errors error', cherrypy.request)
+
+
+cherrypy.tree.mount(robots())
+
+cherrypy.config.update({'error_page.default': error_page_default})
+cherrypy.config.update({'engine.autoreload_on':False})
